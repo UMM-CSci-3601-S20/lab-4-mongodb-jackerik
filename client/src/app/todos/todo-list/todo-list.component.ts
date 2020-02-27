9ad25b6626cb8f484ccef4c1cc40d7374ebbebcc
Jack <pera0064@morris.umn.edu>
2020-02-27 14:26:15 -0600

    Worked on tests for todo.service

--- client/src/app/todos/todo-list/todo-list.component.ts
+++ client/src/app/todos/todo-list/todo-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Todo } from '../todo';
-import { TodoService } from '../todo-service.service';
+import { TodoService } from '../todo.service';
 import { Subscription } from 'rxjs';
 
 @Component({
