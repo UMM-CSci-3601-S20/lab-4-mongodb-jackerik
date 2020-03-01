5f6e619bcad2ff525aa057b0c4ee6c95a30800a5
Jack Perala <peralaj9>
2020-03-01 17:37:13 -0600

    Created the add todo typeScript
    Set up the typescript for the add todo feature as well as set up all validation parameters for the form.

--- client/src/app/todos/add-todo/add-todo.component.ts
+++ client/src/app/todos/add-todo/add-todo.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Todo } from '../todo';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-add-todo',
@@ -7,9 +12,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
-  constructor() { }
+  addTodoForm: FormGroup;
 
-  ngOnInit(): void {
+  todo: Todo;
+
+  constructor(private fb: FormBuilder, private todoService: TodoService, private snackBar: MatSnackBar, private router: Router) {
+  }
+
+  // not sure if this name is magical and making it be found or if I'm missing something,
+  // but this is where the red text that shows up (when there is invalid input) comes from
+  add_todo_validation_messages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
+      {type: 'pattern', message: 'Owner must contain only numbers and letters'},
+      {type: 'existingName', message: 'Owner has already been taken'}
+    ],
+
+    category: [
+      {type: 'required', message: 'Category is required'},
+      {type: 'minlength', message: 'Category must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Category cannot be more than 50 characters long'},
+      {type: 'pattern', message: 'Category must contain only numbers and letters'},
+      ],
+
+      body: [
+        {type: 'maxlength', message: 'Body cannot be more than 50 characters long'},
+      ],
+
+    status: [
+      { type: 'required', message: 'Status is required' },
+      { type: 'pattern', message: 'Status must be Complete, or Incomplete' },
+    ]
+  };
+
+  createForms() {
+
+    // add todo form validations
+    this.addTodoForm = this.fb.group({
+      owner: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50),
+        Validators.pattern('^[A-Za-z0-9\\s]+[A-Za-z0-9\\s]+$(\\.0-9+)?'),
+        (fc) => {
+          if (fc.value.toLowerCase() === 'abc123' || fc.value.toLowerCase() === '123abc') {
+            return ({existingOwner: true});
+          } else {
+            return null;
+          }
+        },
+      ])),
+
+      category: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50),
+        Validators.pattern('^[A-Za-z0-9\\s]+[A-Za-z0-9\\s]+$(\\.0-9+)?'),
+      ])),
+
+      body: new FormControl('', Validators.compose([
+        Validators.maxLength(50),
+      ])),
+
+      status: new FormControl('incomplete', Validators.compose([
+        Validators.required,
+        Validators.pattern('^(complete|incomplete)$'),
+      ])),
+    });
+
+  }
+
+  ngOnInit() {
+    this.createForms();
+  }
+
+
+  submitForm() {
+    this.todoService.addTodo(this.addTodoForm.value).subscribe(newID => {
+      this.snackBar.open('Added Todo ' + this.addTodoForm.value.owner, null, {
+        duration: 2000,
+      });
+      this.router.navigate(['/todo/', newID]);
+    }, err => {
+      this.snackBar.open('Failed to add the todo', null, {
+        duration: 2000,
+      });
+    });
   }
 
 }
